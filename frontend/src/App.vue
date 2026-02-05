dc03bffada9ee8dc1262cafd986912e020632890
Huiwen Qin <1482011017@qq.com>
2026-02-05 22:57:16 +0800

    创建导航栏

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
-
-
+import NavBar from "@/components/navbar/NavBar.vue";
 </script>
 
 <template>
 <!-- 这个组件里面写html -->
+  <NavBar>
+    <!-- NavBar中的内容会直接填充到NavBar.vue中的slot组件中 -->
+    页面内容
+  </NavBar>
 </template>
 
 <style scoped>
---
+++ frontend/src/components/navbar/NavBar.vue
@@ -0,0 +1,87 @@
+<script setup>
+
+import Menuicon from "@/components/navbar/icon/Menuicon.vue";
+import Homepageicon from "@/components/navbar/icon/Homepageicon.vue";
+import Creaticon from "@/components/navbar/icon/Creaticon.vue";
+import Friendicon from "@/components/navbar/icon/Friendicon.vue";
+import Searchicon from "@/components/navbar/icon/Searchicon.vue";
+</script>
+
+<template>
+<div class="drawer lg:drawer-open">
+  <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content">
+    <!-- Navbar -->
+    <nav class="navbar w-full h-14 bg-base-100 shadow-sm">
+      <div class="navbar-start">
+              <label for="my-drawer-4" aria-label="open sidebar" class="btn btn-square btn-ghost">
+                 <!-- 菜单icon -->
+                  <Menuicon/>
+              </label>
+              <div class="px-2 font-bold text-xl">AIFriends</div>
+      </div>
+      <div class="navbar-center w-4/5 max-w-180">
+        <div class="join w-4/5">
+          <input class="input join-item rounded-l-full w-4/5" placeholder="搜索你感兴趣的内容" />
+          <button class="btn join-item rounded-r-full gap-0">
+            <Searchicon/>
+            搜索
+          </button>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <button class="btn btn-neutral btn-outline" text-lg>登录</button>
+      </div>
+
+    </nav>
+    <!-- Page content here -->
+    <slot>
+
+    </slot>
+  </div>
+
+  <div class="drawer-side is-drawer-close:overflow-visible">
+    <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
+    <div class="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-16 is-drawer-open:w-54">
+      <!-- Sidebar content here -->
+      <ul class="menu w-full grow">
+        <!-- 首页 -->
+        <li>
+          <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right flex h-14 items-center gap-2" data-tip="首页" my1="">
+            <!-- 首页icon -->
+            <Homepageicon/>
+            <!-- whitespace-nowrap禁止换行 -->
+            <span class="is-drawer-close:hidden whitespace-nowrap">首页</span>
+          </button>
+        </li>
+
+        <!-- 创作 -->
+        <li>
+          <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right flex h-14 items-center gap-2" data-tip="创作" my1="">
+            <!-- 创作icon -->
+            <Creaticon/>
+            <!-- whitespace-nowrap禁止换行 -->
+            <span class="is-drawer-close:hidden whitespace-nowrap">创作</span>
+          </button>
+        </li>
+
+         <!-- 好友-->
+        <li>
+          <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right flex h-14 items-center gap-2" data-tip="好友" my1="">
+            <!-- 好友icon -->
+            <Friendicon/>
+            <!-- whitespace-nowrap禁止换行 -->
+            <span class="is-drawer-close:hidden whitespace-nowrap">好友</span>
+          </button>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
